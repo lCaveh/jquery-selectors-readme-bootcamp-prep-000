e9455f13017157e54bc60e3a42d569ae67b326bc
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-20 18:16:25 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -7,6 +7,6 @@ function lastImageSelector() {
   return lastImage;
 }
 function ninjaBabySelector() {
-  const ninjaImage = $('.baby-ninja img');
+  const ninjaImage = $('.baby-ninja img src');
   return ninjaImage;
 }
